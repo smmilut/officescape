528d60ced7ddd98e971f3cb91a40c7e7090dcd64
Pil Smmilut <pil.smmilut@gmail.com>
2021-10-26 15:52:18 +0200

    collision WIP

--- www/game/physics.js
+++ www/game/physics.js
@@ -69,6 +69,83 @@ const System_moveMobiles = {
     },
 };
 
+const CollisionRectangle = {
+    init: function CollisionRectangle_init(width, height) {
+        this.size = {
+            x: width,
+            y: height,
+        };
+    },
+    collideRectangle: function CollisionRectangle_collideRectangle(thisPosition, otherCollider, otherPosition) {
+        const thisLeft = thisPosition.x - this.size.x / 2.0;
+        const thisRight = thisPosition.x + this.size.x / 2.0;
+        const thisTop = thisPosition.y - this.size.y / 2.0;
+        const thisBottom = thisPosition.y + this.size.y / 2.0;
+        const otherLeft = otherPosition.x - otherCollider.size.x / 2.0;
+        const otherRight = otherPosition.x + otherCollider.size.x / 2.0;
+        const otherTop = otherPosition.y - otherCollider.size.y / 2.0;
+        const otherBottom = otherPosition.y + otherCollider.size.y / 2.0;
+        if (
+            thisLeft <= otherRight &&
+            thisRight >= otherLeft &&
+            thisBottom >= otherTop &&
+            thisTop <= otherBottom
+        ) {
+            /*
+            TODO : calculate pushback direction
+            const distanceX = thisPosition.x - otherPosition.x;
+            const distanceY = thisPosition.y - otherPosition.y;
+            */
+            return {
+                hasCollision: true,
+            };
+        } else {
+            return {
+                hasCollision: false,
+            };
+        }
+    },
+    collidePoint: function CollisionRectangle_collidePoint(thisPosition, pointPosition) {
+        const thisLeft = thisPosition.x - this.size.x / 2.0;
+        const thisRight = thisPosition.x + this.size.x / 2.0;
+        const thisTop = thisPosition.y - this.size.y / 2.0;
+        const thisBottom = thisPosition.y + this.size.y / 2.0;
+        const distanceToLeft = pointPosition.x - thisLeft;
+        const distanceToRight = pointPosition.x - thisRight;
+        const distanceToBottom = pointPosition.y - thisBottom;
+        const distanceToTop = pointPosition.y - thisTop;
+        if (
+            distanceToLeft >= 0 &&
+            distanceToRight <= 0 &&
+            distanceToBottom <= 0 &&
+            distanceToTop >= 0
+        ) {
+            let pushbackX, pushbackY;
+            if (distanceToLeft < -distanceToRight) {
+                pushbackX = -distanceToLeft;
+            } else {
+                pushbackX = -distanceToRight;
+            }
+            if (distanceToTop < -distanceToBottom) {
+                pushbackY = -distanceToTop;
+            } else {
+                pushbackY = -distanceToBottom;
+            }
+            return {
+                hasCollision: true,
+                pushback: {
+                    pushbackX,
+                    pushbackY,
+                },
+            };
+        } else {
+            return {
+                hasCollision: false,
+            };
+        }
+    },
+};
+
 /** Call when loading */
 export function init(engine) {
     engine.registerResource(Resource_Physics);
